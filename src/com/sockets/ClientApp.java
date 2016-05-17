bb4bd81f661573cd7fdc6cf10b8a33bb5f7a69dd
lrsousa <leonarrangel@gmail.com>
2016-05-17 20:48:56 -0300

    Arrumando user

--- src/com/sockets/ClientApp.java
+++ src/com/sockets/ClientApp.java
@@ -6,5 +6,6 @@ import java.net.UnknownHostException;
 public class ClientApp {
 	 public static void main(String[] args) throws UnknownHostException, IOException {
 		 new Client("127.0.0.1", 4444).executa();
+		 
 	 }
 }
